b893110d762a1d8fe70fc6f5205e32e9ba952583
IamPANDAXX <pandax463@gmail.com>
2025-11-14 00:03:03 -0600

    Primer commit: ocupamos solo la carpeta de juegos

---
+++ gameplayer.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <script src="https://unpkg.com/@ruffle-rs/ruffle"></script>
+</head>
+<body>
+     <object data="flashplayer_games/Fancy Pants.swf" width="800" height="600"></object>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MASTER-RETROS</title>
+    <script src="https://unpkg.com/@ruffle-rs/ruffle"></script>
+
+</head>
+<body>
+      <button onclick="window.location.href='gameplayer.php'">Jugar</button>
+
+
+</body>
+</html>
